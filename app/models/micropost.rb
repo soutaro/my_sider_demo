d282d6fcc1cf7cb14a859f77fd9b0bb649a5b94d
Soutaro Matsumoto <matsumoto@soutaro.com>
2018-10-15 09:58:49 -0700

    Fix validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,9 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :user
 
-  # RuboCop will say "You can use `validates` instead of `validates_*`.".
-  # For example:
-  #   validates :content, presence: true, length: { maximum: 200 }
-  validates_length_of :content, maximum: 200
-  validates_presence_of :content
+  validates :content, presence: true, length: { maximum: 200 }
 end
